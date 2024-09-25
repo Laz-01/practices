deb56f77a6974de9b3eefb3bac88f486912c0470
Maryla <maria.laz4renko@yandex.ru>
2024-09-26 00:04:15 +0300

    Delete pr_2(1).py

--- pr_2(1).py
+++
@@ -1,6 +0,0 @@
-import math
-x=float(6.251)
-y=float(0.827)
-z=float(25.001)
-s=(y**((abs(x))**(1/3)))+(((math.cos(y))**3)*((abs(x-y))*(1+(((math.sin(z))**2)/((x+y)**0.5)))/((math.e**(abs(x-y)))+(x/2))))
-print(s)
